683742f7a47c60aef54c0baccb2db702e63dfa80
adithyarao3103 <64120705+adithyarao3103@users.noreply.github.com>
2023-07-22 15:08:21 +0530

    Add measurements

--- src/bfssmeasurements.h
+++ src/bfssmeasurements.h
@@ -8,11 +8,12 @@
 #define SRC_BFSS_BFSSMEASUREMENTS_H_
 #include "bfsssystemstate.h"
 #include "configparser.h"
+#include "fermionmeasurements.h"
 
 #include <fstream>
 
-class BfssMeasurements{
-  private:
+  class BfssMeasurements{
+  private:  
    std::ofstream outputfile_={};
   public:
     using SystemState=BfssSystemState;
@@ -24,15 +25,28 @@ class BfssMeasurements{
 
     void measure(SystemState& state){
       outputfile_<<state.configNum()<<" ";
+      Complex ferm(bfssFermionicEnergy(state.config(), 20, state.getDiracOperator(), state.getDiracOperatorDagger()));
+      // Complex ferm(0,0);
+      Real bos(bfssBosonicEnergy(state.config()));
+      outputfile_<<ferm.real()<<" "<<ferm.imag()<<" ";
+      outputfile_<<bos<<" ";
+      outputfile_<<ferm.real() + bos<<" ";
       outputfile_<<sumTraceX2Norm(state.config())<<" ";
-      outputfile_<<polyakovLoopAlpha(state.config())<<" ";
+      outputfile_<<traceOfIdentity(state.config(), 20);
       outputfile_<<std::endl;
     }
 
     void printStart(){
+      outputfile_<<"# NUMCOLORS "<<Real(numcolor)<<" ";
+      outputfile_<<"Total Energy Functional";
+      outputfile_<<std::endl;
       outputfile_<<"#1:ConfigNumber ";
-      outputfile_<<"2:X2 ";
-      outputfile_<<"3:PL ";
+      outputfile_<<"2:Fermionic Energy (Real) ";
+      outputfile_<<"3:Fermionic Energy (Imag) ";
+      outputfile_<<"4:Bosonic Energy ";
+      outputfile_<<"5:Total Energy ";
+      outputfile_<<"6:X^2 ";
+      outputfile_<<"7:Tr(I)";
       outputfile_<<std::endl;
     }
 
@@ -40,8 +54,6 @@ class BfssMeasurements{
 
     }
 
-};
-
-
+}; 
 
 #endif /* SRC_BFSS_BFSSMEASUREMENTS_H_ */
